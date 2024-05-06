df689017108c7477dd26694dfa002b3d2ecbcf89
Alejandro Monroy Muñoz <mon@wcw-staff-145-109-82-121.wireless.uva.nl>
2024-05-06 20:15:39 +0200

    Data utils

---
+++ data_utils/celebA_utils.py
@@ -0,0 +1,28 @@
+from torchvision.datasets import CelebA
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+
+def get_dataloader(data_dir='data/', batch_size=4):
+    """
+    Builds a dataloader with all images from the CelebA dataset.
+    Args:
+        data_dir: Directory where the data is stored.
+        batch_size: Size of the batches
+
+    Returns:
+        DataLoader: DataLoader object containing the dataset.
+
+    """
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
+
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+        transforms.Resize((64, 64))
+    ])
+
+    dataset = CelebA(root=data_dir, split='all', download=True, transform=data_transforms)
+
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=True)
---
+++ data_utils/cifar10_utils.py
@@ -0,0 +1,28 @@
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+
+def get_dataloader(data_dir='data/cifar10/', batch_size=16):
+    """
+    Builds a dataloader with all training images from the CIFAR-10 dataset.
+    Args:
+        data_dir: Directory where the data is stored.
+        batch_size: Size of the batches
+
+    Returns:
+        DataLoader: DataLoader object containing the dataset.
+
+    """
+
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
+    dataset = CIFAR10(root=data_dir, train=True, download=True, transform=transform)
+
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=True)
