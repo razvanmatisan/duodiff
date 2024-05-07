ab2ee4972692d21b2a2f136192f229b536094cb1
daniel-gallo <danielsantoangel@gmail.com>
2024-05-07 19:11:46 +0200

    Don't download datasets on GitHub

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,2 +1,4 @@
 from .celeba import get_celeba_dataloader
 from .cifar10 import get_cifar10_dataloader
+
+__all__ = [get_celeba_dataloader, get_cifar10_dataloader]
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,9 +1,24 @@
+from functools import wraps
+from pathlib import Path
+
 import pytest
 import torch
 
 from datasets import get_celeba_dataloader, get_cifar10_dataloader
 
 
+def ignore_if_data_not_downloaded(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not Path("data/").exists():
+            return
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@ignore_if_data_not_downloaded
 @pytest.mark.parametrize("batch_size", [16])
 def test_cifar10(batch_size):
     dataloader = get_cifar10_dataloader(batch_size=batch_size)
@@ -11,10 +26,11 @@ def test_cifar10(batch_size):
     x, _ = next(iter(dataloader))
     assert x.shape == torch.Size([batch_size, 3, 32, 32])
 
+
+@ignore_if_data_not_downloaded
 @pytest.mark.parametrize("batch_size", [4])
-def test_celebA(batch_size):
+def test_celeba(batch_size):
     dataloader = get_celeba_dataloader(batch_size=batch_size)
 
     x, _ = next(iter(dataloader))
     assert x.shape == torch.Size([batch_size, 3, 64, 64])
-
