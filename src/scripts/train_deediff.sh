0ca7e0e89601af0211647544a4e04bb70997660c
JP <github.gaaa9@simplelogin.fr>
2024-05-30 21:25:02 +0200

    Update

--- src/scripts/train_deediff.sh
+++ src/scripts/train_deediff.sh
@@ -1,9 +1,10 @@
+model=deediff_uvit
 number_of_training_steps=100000
 batch_size=128
 classifier_type="attention_probe"
 
 python train.py \
-    --model deediff_uvit \
+    --model ${deediff_uvit} \
     --n_steps ${number_of_training_steps} \
     --batch_size ${batch_size} \
     --classifier_type ${classifier_type} \
