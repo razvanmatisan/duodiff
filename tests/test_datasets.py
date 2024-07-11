247e85d0d1775896794c98aa427f148761b88a3b
Razvan Matisan <razvanmatisan@gmail.com>
2024-07-11 16:15:35 +0300

    Pytest ImageNet dataloader

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -4,13 +4,29 @@ from pathlib import Path
 import pytest
 import torch
 
-from datasets import get_celeba_dataloader, get_cifar10_dataloader
+from datasets import (
+    get_celeba_dataloader,
+    get_cifar10_dataloader,
+    get_imagenet_dataloader,
+)
 
 
 def ignore_if_data_not_downloaded(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         if not Path("data/").exists():
+            return
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+# Might delete later
+def ignore_if_imagenet_data_not_downloaded(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not Path("archive/").exists():
             return
 
         return f(*args, **kwargs)
@@ -34,3 +50,12 @@ def test_celeba(batch_size):
 
     x, _ = next(iter(dataloader))
     assert x.shape == torch.Size([batch_size, 3, 64, 64])
+
+
+@ignore_if_imagenet_data_not_downloaded
+@pytest.mark.parametrize("batch_size", [4])
+def test_imagenet(batch_size):
+    dataloader = get_imagenet_dataloader(batch_size=batch_size, seed=0)
+
+    x, _ = next(iter(dataloader))
+    assert x.shape == torch.Size([batch_size, 3, 256, 256])
