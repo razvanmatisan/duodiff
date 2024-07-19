879ef2f9b1b2b465f787341f9822682448ae4cbc
Razvan Matisan <razvanmatisan@gmail.com>
2024-07-19 18:08:17 +0300

    Scripts for sampling uvit and training deediff, uvit

---
+++ scripts/sample.sh
@@ -0,0 +1,12 @@
+batch_size=4
+config_path=configs/uvit_imagenet_class_cond.yaml
+checkpoint_path=checkpoints/downloaded_checkpoints/imagenet256_uvit_large.pth
+output_folder=samples/imagenet/
+
+python sampler.py \
+    --parametrization predict_noise \
+    --batch_size $batch_size \
+    --seed 1 \
+    --config_path $config_path \
+    --checkpoint_path $checkpoint_path \
+    --output_folder $output_folder
--- scripts/train_deediff.sh
+++ scripts/train_deediff.sh
@@ -1,11 +1,18 @@
 model=deediff_uvit
-number_of_training_steps=100000
+number_of_training_steps=150000
 batch_size=128
-classifier_type="attention_probe"
+config_path=configs/deediff_imagenet_class_cond.yaml
 
-python train.py \
-    --model ${model} \
-    --n_steps ${number_of_training_steps} \
-    --batch_size ${batch_size} \
-    --classifier_type ${classifier_type} \
-    --normalize_timesteps
+CUBLAS_WORKSPACE_CONFIG=:4096:8 python main.py \
+    --model $model \
+    --n_steps $number_of_training_steps \
+    --batch_size $batch_size \
+    --use_amp \
+    --log_every_n_steps 10000 \
+    --save_every_n_steps 10000 \
+    --save_new_every_n_steps 10000 \
+    --seed 1 \
+    --config_path $config_path \
+    --parametrization predict_noise \
+    --dataset imagenet \
+    --freeze_backbone
